b73d1f6d3bd6e4d31bc95a555b095f6ac6d3caaf
shanhaider23 <shan.haider@gmail.com>
2024-12-13 16:39:10 +0100

    completed the app

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://fvbfytipsnzrevqtbcta.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2YmZ5dGlwc256cmV2cXRiY3RhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyMDI5MzIsImV4cCI6MjA0Nzc3ODkzMn0.xiqJzxu5PJZXNg0DPxeeOltk6dUFmHjlnd6B9Rw0s0U";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
